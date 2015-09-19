48711921de1ac852843a5dc18f5e937098215bf4
Krzysztof Szabelski <kszabelski@gmail.com>
2015-09-19 14:35:10 +0200

    Create ConstructionProject feature and remove more code from template

--- AzureConstructionsProgressTracker/App_Start/RouteConfig.cs
+++ AzureConstructionsProgressTracker/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AzureConstructionsProgressTracker
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ConstructionProjects", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
